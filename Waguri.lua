8aca1ba82b7a7d854b5cfd34af4776337c76ae27
Waguriiii <vjramon875@gmail.com>
2026-07-21 20:21:33 +0800

    Update Waguri.lua

--- Waguri.lua
+++ Waguri.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/jntLb8Z2sF/0", true))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/jhq2YCSzX4/0", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Waguriiiii/Murder-mystery-2/refs/heads/main/Waguri.lua"))()
